6aa2899ca8e9ddd0e57450e27be90c76d0e59cb3
David Oberhollenzer <david.oberhollenzer@tele2.at>
2016-12-03 23:33:13 +0100

    Add bandwidth limit to cable
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- cable.c
+++ cable.c
@@ -14,6 +14,29 @@
 static cfg_cable *cables = NULL;
 
 
+static const char *suffix[] = {
+	"bit",
+	"Kibit",
+	"kbit",
+	"mibit",
+	"mbit",
+	"gibit",
+	"gbit",
+	"tibit",
+	"tbit",
+	"Bps",
+	"KiBps",
+	"KBps",
+	"MiBps",
+	"MBps",
+	"GiBps",
+	"GBps",
+	"TiBps",
+	"TBps",
+	NULL
+};
+
+
 static cfg_cable *create_cable(parse_ctx_t *ctx, int lineno, void *parent)
 {
 	cfg_cable *cable = calloc(1, sizeof(*cable));
@@ -29,9 +52,53 @@ static cfg_cable *create_cable(parse_ctx_t *ctx, int lineno, void *parent)
 	return cable;
 }
 
+static int check_cable_port(parse_ctx_t *ctx, int index, int lineno)
+{
+	char buffer[MAX_LIMIT_STR], *ptr;
+	size_t i;
+
+	if (index < 2)
+		return cfg_check_name_arg(ctx, index, lineno);
+
+	if (index == 2) {
+		if (cfg_get_arg(ctx, buffer, sizeof(buffer)))
+			return -1;
+
+		if (strlen(buffer) >= (sizeof(buffer) - 1)) {
+			fprintf(stderr, "%d: bandwidth argument too big\n",
+				lineno);
+			return -1;
+		}
+
+		if (!isdigit(buffer[0])) {
+			fprintf(stderr, "%d: bandwidth must be "
+				"integer value\n", lineno);
+			return -1;
+		}
+		for (ptr = buffer; isdigit(*ptr); ++ptr)
+			;
+
+		if (*ptr) {
+			for (i = 0; suffix[i]; ++i) {
+				if (!strcasecmp(ptr, suffix[i]))
+					break;
+			}
+			if (!suffix[i]) {
+				fprintf(stderr, "%d: unknown suffix '%s'\n",
+					lineno, ptr);
+				return -1;
+			}
+		}
+		return 0;
+	}
+
+	fprintf(stderr, "%d: too many arguments\n", lineno);
+	return -1;
+}
+
 static cfg_node_port *add_port(parse_ctx_t *ctx, int lineno, cfg_cable *cable)
 {
-	char node[MAX_NAME + 1], port[MAX_NAME + 1];
+	char node[MAX_NAME + 1], port[MAX_NAME + 1], limit[MAX_LIMIT_STR];
 	cfg_node_port *p;
 	cfg_token_t tk;
 	cfg_node *n;
@@ -75,6 +142,21 @@ static cfg_node_port *add_port(parse_ctx_t *ctx, int lineno, cfg_cable *cable)
 		return NULL;
 	}
 
+	/* bandwidth limit */
+	ret = cfg_next_token(ctx, &tk, 1);
+	if (ret < 0)
+		goto fail_errno;
+	assert(ret != 0);
+
+	if (tk.id == TK_ARG) {
+		ctx->readoff += sizeof(tk);
+		if (cfg_get_arg(ctx, limit, sizeof(limit)))
+			return NULL;
+	} else {
+		limit[0] = '\0';
+	}
+
+	/* store */
 	if (cable->upper) {
 		if (cable->lower) {
 			fprintf(stderr, "%d: cable cannot have more than "
@@ -82,8 +164,11 @@ static cfg_node_port *add_port(parse_ctx_t *ctx, int lineno, cfg_cable *cable)
 			return NULL;
 		}
 		cable->lower = p;
+
+		strcpy(cable->uplimit, limit);
 	} else {
 		cable->upper = p;
+		strcpy(cable->downlimit, limit);
 	}
 
 	p->connected = 1;
@@ -141,6 +226,19 @@ static int cable_drv_start(driver_t *drv)
 		netns_run(lower->owner->name, "ip link set dev %s up",
 						lower->name);
 
+		/* set limits */
+		if (cable->uplimit[0]) {
+			netns_run(lower->owner->name,
+				"tc qdisc add dev %s root netem rate %s",
+				lower->name, cable->uplimit);
+		}
+
+		if (cable->downlimit[0]) {
+			netns_run(upper->owner->name,
+				"tc qdisc add dev %s root netem rate %s",
+				upper->name, cable->downlimit);
+		}
+
 		/* reconfigure */
 		for (i = 0; i < lower->num_addresses; ++i) {
 			netns_run(lower->owner->name, "ip addr add %s dev %s",
@@ -186,9 +284,9 @@ static int cable_drv_gen_graph(driver_t *drv)
 static parser_token_t cable_tokens[] = {
 	{
 		.keyword = "port",
-		.flags = FLAG_ARG_EXACT,
+		.flags = FLAG_ARG_MIN,
 		.argcount = 2,
-		.argfun = cfg_check_name_arg,
+		.argfun = check_cable_port,
 		.deserialize = (deserialize_fun_t)add_port,
 	}, {
 		.keyword = NULL,
--- cable.h
+++ cable.h
@@ -3,9 +3,14 @@
 
 #include "node.h"
 
+#define MAX_LIMIT_STR 32
+
 typedef struct cfg_cable {
 	cfg_node_port *upper;
 	cfg_node_port *lower;
+
+	char uplimit[MAX_LIMIT_STR];
+	char downlimit[MAX_LIMIT_STR];
 
 	struct cfg_cable *next;
 } cfg_cable;
